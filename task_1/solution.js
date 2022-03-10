499ce3d17c982e32ad448ca886e238b545bd9be1
formary <69672028+formary@users.noreply.github.com>
2022-03-10 15:51:49 +0300

    Add files via upload

---
+++ task_1/solution.js
@@ -0,0 +1,52 @@
+function addInCartMessage(title, price) {
+    let productName = title;
+    let productPrice = price;
+
+    // Задание №1.1. Оповещение о добавлении в корзину
+    let message = productName + ' за ' + productPrice + ' теперь в корзине!';
+    // создайте переменную message
+    // присвойте переменной message значение переменной productName
+    // добавьте в конец переменной message пробел
+    // добавьте в конец переменной message текст 'за'
+    // добавьте в конец переменной message пробел
+    // добавьте в конец переменной message значение переменной productPrice
+    // добавьте в конец переменной message пробел
+    // добавьте в конец переменной message текст 'теперь в корзине!'
+    // выведите переменную message в консоль
+    console.log(message);
+    // Конец решения задания №1.1.
+
+    return message;
+}
+
+function addInCartCountChange(value) {
+
+    // Задание №1.2. Увеличение количества в корзине
+    let oldValue = value;
+    let newValue = oldValue;
+    newValue = newValue + 1;
+    // создайте переменную newValue
+    // присвойте ей значение oldValue
+    // увеличьте значение newValue на 1
+
+    // Конец решения задания №1.2.
+
+    return newValue;
+}
+
+function addInCartSumChange(sum, delta) {
+    let oldSum = sum;
+    let difference = delta;
+    let newSum = oldSum + difference;
+    let newSumText = newSum + ' ₽';
+    // Задание №1.3. Увеличение суммы в корзине
+
+    // создайте переменную newSum и присвойте ей сумму oldSum и difference
+    // создайте переменную newSumText и присвойте ей результат склеивания переменной newSum и текста ' ₽'
+    // замените склеивание, используемое в newSum Text на шаблонную строку
+
+    // Конец решения задания №1.3.
+
+    return newSumText;
+}
+
